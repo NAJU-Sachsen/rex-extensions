b0af18e92ae7b97830e15fbf258b221fb84ea483
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-05-27 11:52:46 +0200

    Use KVS when fetching group information

--- lib/article.php
+++ lib/article.php
@@ -7,11 +7,28 @@ class naju_article
     public const GROUP_PREFIX = 'NAJU ';
     public const DEFAULT_LOGO = 'naju-logo.png';
 
+    /**
+     * Searches for the name of the local group to which the currently requested article belongs.
+     *
+     * The local group's name will be provided without prefix, i.e. if the group is called
+     * "NAJU Dresden", then "Dresden" will be returned. Furthermore, if the requested article
+     * is not part of the group article hierarchy, then the default group will be provided.
+     */
     public static function determineCurrentLocalGroup()
     {
         $detected_group = '';
         $category = rex_category::getCurrent();
 
+        // firstly, check the KVS if there already is a group name stored
+        // for the requested category
+        $kvs_group = naju_kvs::get('category.localgroup.' . $category);
+        if ($kvs_group) {
+            return $kvs_group;
+        }
+
+        // if not, traverse the category hierarchy to check if we are in the local-groups subtree
+        // the categories directly underneath the 'Ortsgruppen' category will be the ones containing
+        // the requested local group
         while ($category) {
             $parent = $category->getParent();
             if ($parent && $parent->getName() == self::GROUP_ROOT_CATEGORY) {
@@ -21,9 +38,20 @@ class naju_article
             $category = $parent;
         }
 
-        return $detected_group ? self::dropLeadingPrefix($detected_group) : self::DEFAULT_GROUP;
+        // finally, if we found the group, store it in the KVS, otherwise fall back to our default group
+        if ($detected_group) {
+            $group_name = self::dropLeadingPrefix($detected_group);
+            naju_kvs::put('category.localgroup.' . $category, $group_name);
+            return $group_name;
+        } else {
+            return self::DEFAULT_GROUP;
+        }
     }
 
+    /**
+     * Provides the name of the logo file for the local group which owns the currently
+     * requested article.
+     */
     public static function getLogoForCurrentLocalGroup()
     {
         $local_group = self::determineCurrentLocalGroup();
@@ -54,6 +82,10 @@ class naju_article
         }
     }
 
+    /**
+     * Removes the common 'NAJU' prefix from a local group name, retaining only the
+     * actual group portion.
+     */
     private static function dropLeadingPrefix($group)
     {
         if (!$group) {
@@ -63,6 +95,9 @@ class naju_article
         return substr($group, strpos($group, self::GROUP_PREFIX)+strlen(self::GROUP_PREFIX));
     }
 
+    /**
+     * Provides a unified format for local group names to use in logo images.
+     */
     private static function generateLogoNameForLocalGroup($group)
     {
         return rex_string::normalize($group);
