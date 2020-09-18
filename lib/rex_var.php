a0367e88a98d211ff667e4c874d3215a69f34b5c
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-09-18 16:09:21 +0200

    Support custom flags in rex_var::toArray

--- lib/rex_var.php
+++ lib/rex_var.php
@@ -2,9 +2,9 @@
 
 class naju_rex_var
 {
-		public static function toArray($value)
+		public static function toArray($value, $flags = ENT_QUOTES)
 		{
-			$value = json_decode(htmlspecialchars_decode($value, ENT_QUOTES), true);
+			$value = json_decode(htmlspecialchars_decode($value, $flags), true);
 			return is_array($value) ? $value : null;
 		}
 }
