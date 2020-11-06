6563e06ec366f212ec10ef60e4c9a81798a957cd
Rico Bergmann <rico.bergmann1@tu-dresden.de>
2020-11-06 12:58:44 +0100

    Fix EP registration parameters

--- boot.php
+++ boot.php
@@ -5,12 +5,12 @@ if (rex::isBackend() && rex::getUser()) {
     // TODO: refuse image upload if it is too large
 
     // create optimized versions of uploaded images
-    rex_extension::register('MEDIA_ADDED', [function ($ep) {
+    rex_extension::register('MEDIA_ADDED', function ($ep) {
         $file = $ep->getSubject()['filename'];
         if (naju_image::supportedFile($file)) {
             naju_image::createOptimizedVersions($file);
         }
-    }]);
+    });
 
     // also update optimzed versions when base image is updated
     rex_extension::register('MEDIA_UPDATED', function ($ep) {
